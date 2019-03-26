3454f7dd86c4366ac0480831350044a296829822
yashkhade <ykhade@nevada.unr.edu>
2019-03-26 15:53:14 -0700

    Setup Initial routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
+import Navbar from './Navbar';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        React Router Course
-      </div>
+        <Router>
+          <div>
+            <Navbar/>
+            <Route path={'/'} exact component={Home}/>
+            <Route path={'/players'} component={Players}/>
+            <Route path={'/teams'} component={Teams}/>
+          </div>
+        </Router>
     );
   }
 }
