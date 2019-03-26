13f0c64b0bf2c7931805eca24eae8567f7c8a305
yashkhade <ykhade@nevada.unr.edu>
2019-03-26 16:06:10 -0700

    Added (404) route.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Home';
 import Players from './Players';
 import Teams from './Teams';
@@ -7,18 +7,21 @@ import Navbar from './Navbar';
 
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-          <div>
-            <Navbar/>
-            <Route path={'/'} exact component={Home}/>
-            <Route path={'/players'} component={Players}/>
-            <Route path={'/teams'} component={Teams}/>
-          </div>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Navbar/>
+                    <Switch>
+                        <Route path={'/'} exact component={Home}/>
+                        <Route path={'/players'} component={Players}/>
+                        <Route path={'/teams'} component={Teams}/>
+                        <Route render={() => <h1 className={'text-center'}>Four oh Four. </h1>}/>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
