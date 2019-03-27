41dab9cb8b4d0731115a095ff0cb6270294b4cb0
yashkhade <ykhade@nevada.unr.edu>
2019-03-26 18:55:22 -0700

    Rendering Players

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,4 +1,4 @@
-import React, { Component} from "react";
+import React, {Component} from "react";
 import {Route, Link} from 'react-router-dom';
 import Sidebar from './Sidebar';
 import {getPlayers} from "../api";
@@ -10,6 +10,7 @@ export default class Players extends Component {
         players: [],
         loading: true,
     }
+
     componentDidMount() {
         const {location} = this.props
         location.search
@@ -20,13 +21,14 @@ export default class Players extends Component {
     fetchPlayers = (teamId) => {
         getPlayers(teamId)
             .then((players) => this.setState(() => ({
-                loading:false,
+                loading: false,
                 players,
             })))
     }
+
     render() {
         const {players, loading} = this.state;
-        const { match, location} = this.props;
+        const {match, location} = this.props;
         return (
             <div className={'container two-column'}>
                 <Sidebar
@@ -37,8 +39,55 @@ export default class Players extends Component {
                 />
 
                 {loading === false && location.pathname === '/players'
-                ? <div className='sidebar-instruction'>Select a Player</div>
-                : null}
+                    ? <div className='sidebar-instruction'>Select a Player</div>
+                    : null}
+
+                <Route path={`${match.url}/:playerId`}
+                       render={({match}) => {
+                           if (loading === true) return null
+
+                           const {
+                               name, position, teamId, number, avatar, apg, ppg, rpg, spg,
+                           } = players.find((player) => slug(player.name) === match.params.playerId)
+
+                           return (
+                               <div className={'panel'}>
+                                   <img className={'avatar'} src={`${avatar}`} alt={`${name}'s avatar`}/>
+                                   <h1 className={'medium-header'}>{name}</h1>
+                                   <h3 className={'header'}>#{number}</h3>
+                                   <div className={'row'}>
+                                       <ul className={'info-list'} style={{marginRight: 80}}>
+                                           <li>Team
+                                               <div>
+                                                   <Link style={{color: '#68809a'}} to={`/${teamId}`}>
+                                                       {teamId[0].toUpperCase() + teamId.slice(1)}
+                                                   </Link>
+                                               </div>
+                                           </li>
+                                           <li>Position
+                                               <div>{position}</div>
+                                           </li>
+                                           <li>PPG
+                                               <div>{ppg}</div>
+                                           </li>
+                                       </ul>
+                                       <ul className={'info-list'}>
+                                           <li>APG
+                                               <div>{apg}</div>
+                                           </li>
+                                           <li>SPG
+                                               <div>{spg}</div>
+                                           </li>
+                                           <li>RPG
+                                               <div>{rpg}</div>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+
+                           )
+                       }}
+                />
             </div>
         )
     }
