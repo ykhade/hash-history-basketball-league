124c980c78b63de7bddcef3679b4b7e2d7b25238
yashkhade <ykhade@nevada.unr.edu>
2019-03-26 22:30:06 -0700

    Added code splitting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
 import Navbar from './Navbar';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
+
+import DynamicImport from './DynamicImport';
+import Loading from "./Loading";
+
+const Home = (props) => (
+    <DynamicImport load={() => import('./Home')}>
+        {(Component) => Component === null
+            ? <Loading/>
+            : <Component {...props} />}
+    </DynamicImport>
+);
+const Players = (props) => (
+    <DynamicImport load={() => import('./Players')}>
+        {(Component) => Component === null
+            ? <Loading/>
+            : <Component {...props} />}
+    </DynamicImport>
+);
+const Teams = (props) => (
+    <DynamicImport load={() => import('./Teams')}>
+        {(Component) => Component === null
+            ? <Loading/>
+            : <Component {...props} />}
+    </DynamicImport>
+);
+const TeamPage = (props) => (
+    <DynamicImport load={() => import('./TeamPage')}>
+        {(Component) => Component === null
+            ? <Loading/>
+            : <Component {...props} />}
+    </DynamicImport>
+);
+const Articles = (props) => (
+    <DynamicImport load={() => import('./Articles')}>
+        {(Component) => Component === null
+            ? <Loading/>
+            : <Component {...props} />}
+    </DynamicImport>
+);
 
 
 class App extends Component {
