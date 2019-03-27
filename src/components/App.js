eccbebb5d197069582a49a8d6f81f0c915306301
yashkhade <ykhade@nevada.unr.edu>
2019-03-26 21:09:12 -0700

    Rendered/Loaded team articles.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Players from './Players';
 import Teams from './Teams';
 import Navbar from './Navbar';
 import TeamPage from './TeamPage';
+import Articles from './Articles';
 
 
 class App extends Component {
@@ -17,7 +18,8 @@ class App extends Component {
                         <Route path={'/'} exact component={Home}/>
                         <Route path={'/players'} component={Players}/>
                         <Route path={'/teams'} component={Teams}/>
-                        <Route path={'/:teamId'} exact component={TeamPage}/>
+                        <Route path={'/:teamId/'} exact component={TeamPage}/>
+                        <Route path={'/:teamId/articles'} component={Articles}/>
                         <Route render={() => <h1 className={'text-center'}>Four oh Four. </h1>}/> (//Default 404 route)
                     </Switch>
                 </div>
---
+++ src/components/Articles.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import Article from './Article'
+import { getTeamsArticles } from '../api'
+
+export default class Articles extends Component {
+    state = {
+        teamsArticles: [],
+        loading: true,
+    }
+    componentDidMount() {
+        getTeamsArticles(this.props.match.params.teamId)
+            .then((teamsArticles) => {
+                this.setState(() => ({
+                    loading: false,
+                    teamsArticles: teamsArticles.map((article) => article.title),
+                }))
+            })
+    }
+    render() {
+        const { loading, teamsArticles } = this.state
+        const { params, url } = this.props.match
+        const  { teamId } = params
+
+        return loading === true
+            ? <h1>Loading</h1>
+            : <div className='container two-column'>
+                <Sidebar
+                    loading={loading}
+                    title='Articles'
+                    list={teamsArticles}
+                    {...this.props}
+                />
+
+                <Route path={`${url}/:articleId`} render={({ match }) => (
+                    <Article articleId={match.params.articleId} teamId={teamId}>
+                        {(article) => !article ? <h1>Loading</h1> : (
+                            <div className='panel'>
+                                <article className='article' key={article.id}>
+                                    <h1 className='header'>{article.title}</h1>
+                                    <p>{article.body}</p>
+                                </article>
+                            </div>
+                        )}
+                    </Article>
+                )}/>
+            </div>
+    }
+}
