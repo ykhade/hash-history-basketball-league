31c2a5b444af84db04013160d7de989e63cfda70
yashkhade <ykhade@nevada.unr.edu>
2019-03-26 22:07:41 -0700

    Added animated transitions when switching between players

--- src/components/Players.js
+++ src/components/Players.js
@@ -4,6 +4,7 @@ import Sidebar from './Sidebar';
 import {getPlayers} from "../api";
 import {parse} from 'query-string';
 import slug from 'slug';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 export default class Players extends Component {
     state = {
@@ -51,39 +52,44 @@ export default class Players extends Component {
                            } = players.find((player) => slug(player.name) === match.params.playerId)
 
                            return (
-                               <div className={'panel'}>
-                                   <img className={'avatar'} src={`${avatar}`} alt={`${name}'s avatar`}/>
-                                   <h1 className={'medium-header'}>{name}</h1>
-                                   <h3 className={'header'}>#{number}</h3>
-                                   <div className={'row'}>
-                                       <ul className={'info-list'} style={{marginRight: 80}}>
-                                           <li>Team
-                                               <div>
-                                                   <Link style={{color: '#68809a'}} to={`/${teamId}`}>
-                                                       {teamId[0].toUpperCase() + teamId.slice(1)}
-                                                   </Link>
-                                               </div>
-                                           </li>
-                                           <li>Position
-                                               <div>{position}</div>
-                                           </li>
-                                           <li>PPG
-                                               <div>{ppg}</div>
-                                           </li>
-                                       </ul>
-                                       <ul className={'info-list'}>
-                                           <li>APG
-                                               <div>{apg}</div>
-                                           </li>
-                                           <li>SPG
-                                               <div>{spg}</div>
-                                           </li>
-                                           <li>RPG
-                                               <div>{rpg}</div>
-                                           </li>
-                                       </ul>
-                                   </div>
-                               </div>
+                               <TransitionGroup className={'panel'}>
+                                   <CSSTransition key={location.key} timeout={250} classNames={'fade'}>
+                                       <div className={'panel'}>
+                                           <img className={'avatar'} src={`${avatar}`} alt={`${name}'s avatar`}/>
+                                           <h1 className={'medium-header'}>{name}</h1>
+                                           <h3 className={'header'}>#{number}</h3>
+                                           <div className={'row'}>
+                                               <ul className={'info-list'} style={{marginRight: 80}}>
+                                                   <li>Team
+                                                       <div>
+                                                           <Link style={{color: '#68809a'}} to={`/${teamId}`}>
+                                                               {teamId[0].toUpperCase() + teamId.slice(1)}
+                                                           </Link>
+                                                       </div>
+                                                   </li>
+                                                   <li>Position
+                                                       <div>{position}</div>
+                                                   </li>
+                                                   <li>PPG
+                                                       <div>{ppg}</div>
+                                                   </li>
+                                               </ul>
+                                               <ul className={'info-list'}>
+                                                   <li>APG
+                                                       <div>{apg}</div>
+                                                   </li>
+                                                   <li>SPG
+                                                       <div>{spg}</div>
+                                                   </li>
+                                                   <li>RPG
+                                                       <div>{rpg}</div>
+                                                   </li>
+                                               </ul>
+                                           </div>
+                                       </div>
+                                   </CSSTransition>
+                               </TransitionGroup>
+
 
                            )
                        }}
